03318a5b21feb793e25e87f77b77529ba60474bd
mariotey <tey.mingchuan@yahoo.com.sg>
2022-12-09 02:12:09 +0800

    Added JavaScript Mediapipe

--- Walk-E/main_flask.py
+++ Walk-E/main_flask.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
             
 @app.route('/')
 def home():
-    return "I will be writing a new homepage for Walk-E."
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=False)

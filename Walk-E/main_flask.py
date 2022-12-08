93e5e1ed6a795a475076513c8c32e77058fe7917
mariotey <tey.mingchuan@yahoo.com.sg>
2022-12-08 23:11:43 +0800

    Updated dependencies

--- Walk-E/main_flask.py
+++ Walk-E/main_flask.py
@@ -2,28 +2,11 @@ import cv2
 from flask import Flask, render_template, Response
 
 video = cv2.VideoCapture(0)
-app = Flask('__name__')
-
-def video_stream():
-    while True:
-        ret, frame = video.read()
-        if not ret:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpeg', frame)
-            frame = buffer.tobytes()
-            yield(b' --frame\r\n' b'Content-type:image/jpeg\r\n\r\n' + frame +b'\r\n')
+app = Flask(__name__)
             
 @app.route('/')
 def home():
-    return "This is the homepage of the Webpage of Walk-E."
+    return "I will be writing a new homepage for Walk-E."
 
-@app.route('/camera')
-def camera():
-    return render_template("camera.html")
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-app.run(port=5000, debug=False)
+if __name__ == "__main__":
+    app.run(port=5000, debug=False)

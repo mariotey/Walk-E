87ca9611231be7b20d6bc4eeb7d50be7ad8f0cc1
mariotey <tey.mingchuan@yahoo.com.sg>
2022-12-20 20:20:55 +0800

    Updated code_example

--- code_example/raspberry_videostream/camera.py
+++ code_example/raspberry_videostream/camera.py
@@ -1,30 +1,32 @@
 import cv2
-from flask import Flask, Response, render_template, stream_with_context, request
+import numpy
+from flask import Flask, render_template, Response, stream_with_context, request
 
-app = Flask(__name__)
-video = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
+app = Flask('__name__', template_folder='./code_example/raspberry_videostream/templates')
+
 
 def video_stream():
     while True:
+        
         # Read camera frame
-        ret, frame = video.read()
+        ret, frame = camera.read()
         if not ret:
             break
         else:
             ret, buffer = cv2.imencode('.jpeg',frame)
             frame = buffer.tobytes()
 
-            # return only return one image
-            yield (b' --frame\r\n' 
-                    b'Content-type: imgae/jpeg\r\n\r\n' + frame + b'\r\n')
+        # "return" will only return one image
+        yield (b' --frame\r\n' b'Content-type: imgae/jpeg\r\n\r\n' + frame +b'\r\n')
 
-@app.route('/video')
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/')
+@app.route('/video')
 def video():
     return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(port='5000', debug=False)

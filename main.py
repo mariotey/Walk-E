618b107b008f63ea3e2aacb338c69f0800e5cf55
mctey <tey.mingchuan@yahoo.com.sg>
2022-12-21 15:52:49 +0800

    Reorganized files for Walk-E

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose 
 
 camera = cv2.VideoCapture(0)
-app = Flask("__name__", template_folder='./main/templates')
+app = Flask("__name__")
 
 def video_stream():    
     while True:
         # Setting up Pose Estimation Model
         with mp_pose.Pose(min_detection_confidence=0.5,
                         min_tracking_confidence=0.5,
-                        enable_segmentation=True,
+                        enable_segmentation=False,
                         smooth_segmentation=True,
                         smooth_landmarks=True,
                         static_image_mode=False) as pose:
-
+        
             # Read camera frame
             ret, frame = camera.read()
             if not ret:
-                break
+                ret, buffer = cv2.imencode('.jpeg', frame)
             else:
                 # Recolour Image feed (from openCV) from BGR to RGB
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -51,13 +51,13 @@ def video_stream():
                                                                 thickness=2,
                                                                 circle_radius=0)
                                         )
-                    
-                ret, buffer = cv2.imencode('.jpeg',image)
-                frame = buffer.tobytes()
 
-                # "return" will only return one image
-                yield (b'--frame\r\n' 
-                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                ret, buffer = cv2.imencode('.jpeg', image)
+                
+            frame = buffer.tobytes()
+            # "return" will only return one image
+            yield (b'--frame\r\n' 
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -72,12 +72,15 @@ def start():
     request_data = request.form
 
     if request_data["data"] == 'true':
-        # motor.drive(10, 99.9, 100)
+        motor.drive(10, 99.9, 100)
         print("Walk-E has moved.")
     else:
-        # motor.stop()
+        motor.stop()
         print("Walk-E stopped.")
 
     return render_template('main.html')
 
 app.run(port='5000', debug=False)
+
+
+                    

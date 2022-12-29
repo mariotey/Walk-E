c36569b2aa9efa658e3f4bea77640dad8161ddcd
mctey <tey.mingchuan@yahoo.com.sg>
2022-12-29 16:09:03 +0800

    Added redis and motor

--- rasp_pi/suscriber.py
+++ rasp_pi/suscriber.py
@@ -1,15 +1,16 @@
 import redis
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='192.168.1.69', port=6379, db=0)
 
-subscribe_key="walkEStats"
+print(redis_client.keys())
+# subscribe_key="walkEStats"
 
-print(redis_client.get(subscribe_key).decode('utf-8'))
+# redis_data = redis_client.keys()
 
-while True:
-    if redis_client.get(subscribe_key).decode('utf-8') == "false":
-        pass
-    else:
-        break
+# while True:
+#     if redis_client.get(subscribe_key).decode('utf-8') == "false":
+#         pass
+#     else:
+#         break
     
 print("Complete")
--- server/main_flask.py
+++ server/main_flask.py
@@ -5,6 +5,7 @@ import redis
 import gait_calibrate
 import gait_statistics
 import format_data
+import motor
 
 app = Flask(__name__)
 
@@ -36,14 +37,16 @@ def get_stats():
     redis_client = redis.Redis(host="localhost", port=6379)
 
     # Cache joint_data into server
-    redis_client.hset("testjoint_data", "pose_lm", request_data["poseLandmark"])
-    redis_client.hset("testjoint_data", "world_lm", request_data["worldLandmark"])
-    redis_client.hset("testjoint_data", "time", request_data["time"])
+    # redis_client.hset("testjoint_data", "pose_lm", request_data["poseLandmark"])
+    # redis_client.hset("testjoint_data", "world_lm", request_data["worldLandmark"])
+    # redis_client.hset("testjoint_data", "time", request_data["time"])
 
     if request_data["stats"] == 'true':
+        motor.drive(10, 99.9, 100)
         print("Walk-E has moved.")
         
     else:
+        motor.stop()
         print("Walk-E stopped.")
 
     redis_client.set("walkEStats", request_data["stats"])

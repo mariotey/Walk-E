91842e2a83d4844c3babfe710bd973223e1cc530
mctey <tey.mingchuan@yahoo.com.sg>
2023-02-15 11:39:58 +0800

    Optimized Caching

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, request
 import time
 # import RPi.GPIO as GPIO
-# import mediapipe as mp
+import mediapipe as mp
 import cv2
 
 import rediscache
-import gait_calibrate
 import gait_statistics
 import gait_process
-import hardware
+# import hardware
 
 # OP_ENCODE_ONE = 11 #GPIO 17
 # OP_ENCODE_TWO = 36 #GPIO 16
 
 # Pose Estimation Model
-# mp_pose = mp.solutions.pose  
+mp_pose = mp.solutions.pose  
 
-# pose = mp.solutions.pose.Pose(min_detection_confidence=0.5,
-#                     min_tracking_confidence=0.5,
-#                     enable_segmentation=True,
-#                     smooth_segmentation=True,
-#                     smooth_landmarks=True,
-#                     static_image_mode=False)
+pose = mp.solutions.pose.Pose(min_detection_confidence=0.5,
+                    min_tracking_confidence=0.5,
+                    enable_segmentation=True,
+                    smooth_segmentation=True,
+                    smooth_landmarks=True,
+                    static_image_mode=False)
 
 # Get Realtime Webcam Feed
 cap = cv2.VideoCapture(0) 
@@ -46,21 +45,19 @@ def calibrate_hiplen():
     ret, frame = cap.read()
     results = pose.process(frame)
 
-    try:
-        camera_lm = results.pose_landmarks.landmark
-        calibration_hiplen.append(hardware.hip_detect(camera_lm))        
+    # try:
+    #     camera_lm = results.pose_landmarks.landmark
+    #     calibration_hiplen.append(hardware.hip_detect(camera_lm))        
 
-    except AttributeError:
-        # print("Nothing / Errors detected")
-        pass  # Pass if there is no detection or error 
+    # except AttributeError:
+    #     # print("Nothing / Errors detected")
+    #     pass  # Pass if there is no detection or error 
 
     return ('', 204)
 
-@app.route('/Recalibrate', methods=["GET", "POST"])
-def recalibrate():
+@app.route('/CacheCalibrate', methods=["GET", "POST"])
+def cache_calibrate():
     request_data = request.form
-
-    # Cache new calibration data into server 
     rediscache.cache_lm("calibration_data", request_data)
 
     return render_template("main.html")
@@ -69,102 +66,65 @@ def recalibrate():
 
 @app.route('/walkE_move', methods=["GET", "POST"])
 def walkE_move():
-    # ret, frame = cap.read()
-    # results = pose.process(frame)
-
-    try:
-        # camera_lm = results.pose_landmarks.landmark
-        # dist_status = hardware.proxy_detect(frame, camera_lm)
-        
-        # hardware.motor_drive(*walkE_dict.proxy_status[dist_status])
-        hardware.motor_drive(*[25,25])
-                    
-        # stateCount_one, stateLast_one = hardware.encoder_stateChange(OP_ENCODE_ONE, stateCount_one, stateLast_one)
-        # stateCount_two, stateLast_two = hardware.encoder_stateChange(OP_ENCODE_TWO, stateCount_two, stateLast_two)
-        
-    except AttributeError:
-        # print("Nothing / Errors detected")
-        pass  # Pass if there is no detection or error 
-
-    return ('', 204)
-
-@app.route('/walkE_stop', methods=["GET", "POST"])
-def walkE_stop():
-    hardware.motor_drive(*[0,0])
-    return('', 204)
-
-@app.route('/GetStats', methods=["GET", "POST"])
-def get_stats():  
-    request_data = request.form   
-
-    # global move_stats
-
-    # if request_data["stats"] == "true": 
-    #     move_stats = True
-    # else:
-    #     move_stats = False
-        
-    #     # Cache joint_data into server
-    rediscache.cache_lm("testjoint_data", request_data)
-    
     # stateCount_one, stateCount_two = 0, 0
     # stateLast_one, stateLast_two = GPIO.input(OP_ENCODE_ONE), GPIO.input(OP_ENCODE_TWO)
     
     # start_time = time.time()
+    
+    # ret, frame = cap.read()
+    # results = pose.process(frame)
 
-    # while move_stats: 
-    #     ret, frame = cap.read()
-    #     results = pose.process(frame)
+    # try:
+    #     # camera_lm = results.pose_landmarks.landmark
+    #     # dist_status = hardware.proxy_detect(frame, camera_lm)
+        
+    #     # hardware.motor_drive(*walkE_dict.proxy_status[dist_status])
+    #     hardware.motor_drive(*[25,25])
+                    
+    #     # stateCount_one, stateLast_one = hardware.encoder_stateChange(OP_ENCODE_ONE, stateCount_one, stateLast_one)
+    #     # stateCount_two, stateLast_two = hardware.encoder_stateChange(OP_ENCODE_TWO, stateCount_two, stateLast_two)
+        
+    # except AttributeError:
+    #     # print("Nothing / Errors detected")
+    #     pass  # Pass if there is no detection or error 
 
-    #     try:
-    #         print("Walk-E moves")
-
-    #         camera_lm = results.pose_landmarks.landmark
-    #         dist_status = hardware.proxy_detect(frame, camera_lm)
-
-    #         print(dist_status)
-
-    #         # motor_drive(*walkE_dict.proxy_status[dist_status])
-
-    #         # hardware.motor_drive(*[25,25])
-                        
-    #         stateCount_one, stateLast_one = hardware.encoder_stateChange(OP_ENCODE_ONE, stateCount_one, stateLast_one)
-    #         stateCount_two, stateLast_two = hardware.encoder_stateChange(OP_ENCODE_TWO, stateCount_two, stateLast_two)
-            
-    #     except AttributeError:
-    #         # print("Nothing / Errors detected")
-    #         pass  # Pass if there is no detection or error   
-
-    # hardware.motor_drive(0, 0)
     # end_time = time.time()
 
     # print("Walk-E stops")
 
     # hardware.encoder_logic(stateCount_one, stateCount_two, end_time - start_time)
+
+    return ('', 204)
+
+@app.route('/walkE_stop', methods=["GET", "POST"])
+def walkE_stop():
+    # hardware.motor_drive(*[0,0])
+    return('', 204)
+
+@app.route('/CacheStats', methods=["GET", "POST"])
+def cache_stats():  
+    request_data = request.form   
+    rediscache.cache_lm("testjoint_data", request_data)   
     
     return render_template("main.html")
 
 #################################################################################################
 
-@app.route('/PlotStats', methods=["GET", "POST"])
+@app.route('/GetStats', methods=["GET", "POST"])
 def plot_stats():
     # Retrieve calibration and stats data from server 
-    joint_world_lm, joint_time = rediscache.request_lm("testjoint_data")
-    joint_data = gait_process.get_lm(joint_world_lm, joint_time) 
+    joint_data = rediscache.request_lm("testjoint_data")
 
     if joint_data == []:
         return render_template("main.html")
     
-    calibrate_world_lm, calibrate_time = rediscache.request_lm("calibration_data")
-    calibrate_data = gait_process.get_lm(calibrate_world_lm, calibrate_time)
-    offsetdata = gait_calibrate.calibrate(calibrate_data)
+    offsetdata = rediscache.request_lm("calibration_data")
 
     hardware_one_data = rediscache.request_hw("testjoint_data", "hardware_one")
     hardware_two_data = rediscache.request_hw("testjoint_data", "hardware_two")
 
-    print(hardware_one_data)
-    print(hardware_two_data)
-    
+    print(hardware_one_data, hardware_two_data)
+        
     # Calculation of Gait Statistics
     gait_data = gait_process.get_gait(offsetdata["cut_off"], joint_data)
     stats_data = gait_statistics.stats(joint_data, gait_data, offsetdata)
--- rediscache.py
+++ rediscache.py
@@ -1,21 +1,40 @@
 import redis
 import json
 
+import gait_process
+import gait_calibrate
+
 redis_client = redis.Redis(host="localhost", port=6379)
 
+#################################################################################################
+
 def cache_lm(key, request_data):
-    redis_client.hset(key, "pose_lm", request_data["poseLandmark"])
-    redis_client.hset(key, "world_lm", request_data["worldLandmark"])
-    redis_client.hset(key, "time", request_data["time"])
+    pose_lm = json.loads(request_data["poseLandmark"])
+    world_lm = json.loads(request_data["worldLandmark"])
+    time_lm = json.loads(request_data["time"])
+
+    processed_gait = gait_process.get_lm(world_lm, time_lm)
+
+    if key == "calibration_data":
+        offset = gait_calibrate.calibrate(processed_gait)
+        redis_client.hset(key, "offset", json.dumps(offset).encode("utf-8"))
+    
+    if key == "testjoint_data":
+        redis_client.hset(key, "joints", json.dumps(processed_gait).encode("utf-8"))
+
+def request_lm(key):
+    if key == "calibration_data":
+        data = json.loads(redis_client.hget(key, "offset").decode("utf-8"))
+    
+    if key == "testjoint_data":
+        data = json.loads(redis_client.hget(key, "joints").decode("utf-8"))
+    
+    return data
+
+#################################################################################################
 
 def cache_hw(key, dictkey, dict_data):
     redis_client.hset(key, dictkey, json.dumps(dict_data).encode("utf-8"))
-
-def request_lm(key):
-    world_lm = json.loads(redis_client.hget(key, "world_lm").decode("utf-8"))
-    time = json.loads(redis_client.hget(key, "time").decode("utf-8"))
-
-    return world_lm, time
 
 def request_hw(key, dictkey):
     try:
@@ -27,3 +46,5 @@ def request_hw(key, dictkey):
         }
 
     return data
+
+#################################################################################################
